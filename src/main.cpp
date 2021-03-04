9eafd5defda52e328672f95f7331d0b2adb3e7b9
Fr4nt1x <Fr4nt1x@Muc.de>
2021-03-04 19:13:51 +0100

    Added servo library to ps4 controller

--- src/main.cpp
+++ src/main.cpp
@@ -17,76 +17,60 @@
 
 #include "Arduino.h"
 #include "heltec.h"
+#include <ESP32Servo.h> 
 #include <ps4.h>
 #include <PS4Controller.h>
 #include <ps4_int.h>
 
-#define BAND    868E6  //you can set band here directly,e.g. 868E6,915E6
-#define DEMO_DURATION 3000
-typedef void (*Demo)(void);
+Servo myservo;  // create servo object to control a servo
+
+// Possible PWM GPIO pins on the ESP32: 0(used by on-board button),2,4,5(used by on-board LED),12-19,21-23,25-27,32-33 
+int servoPin = 2;      // GPIO pin used to connect the servo control (digital out)
 
 int demoMode = 0;
 int counter = 1;
 
 void setup() {
-  Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Enable*/, true /*Serial Enable*/, false /*LoRa use PABOOST*/, BAND /*LoRa RF working band*/);
+  Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Enable*/, true /*Serial Enable*/, false /*LoRa use PABOOST*/);
   delay(300);
   Heltec.display -> clear();
   delay(100);
   PS4.begin("a8:3e:0e:fa:7c:e7");
   Heltec.display ->drawString(14,0,"Ready.");
   Heltec.display->display();
-}
+    // Allow allocation of all timers
+	ESP32PWM::allocateTimer(0);
+	ESP32PWM::allocateTimer(1);
+	ESP32PWM::allocateTimer(2);
+	ESP32PWM::allocateTimer(3);
+  myservo.setPeriodHertz(50);// Standard 50hz servo
+  myservo.attach(servoPin, 500, 2400);   // attaches the servo on servoPin to the servo object
+                                         // using SG90 servo min/max of 500us and 2400us
+                                         // for MG995 large servo, use 1000us and 2000us,
+                                         // which are the defaults, so this line could be
+                                         // "myservo.attach(servoPin);"
 
+}
+uint steering = 90;
 
 void loop()
 {
-      if (PS4.Right()) Serial.println("Right Button");
-    if (PS4.Down()) Serial.println("Down Button");
-    if (PS4.Up()) Serial.println("Up Button");
-    if (PS4.Left()) Serial.println("Left Button");
+  Heltec.display -> clear();
+    if (PS4.isConnected()){
 
-    if (PS4.Square()) Serial.println("Square Button");
-    if (PS4.Cross()) Serial.println("Cross Button");
-    if (PS4.Circle()) Serial.println("Circle Button");
-    if (PS4.Triangle()) Serial.println("Triangle Button");
-
-    if (PS4.UpRight()) Serial.println("Up Right");
-    if (PS4.DownRight()) Serial.println("Down Right");
-    if (PS4.UpLeft()) Serial.println("Up Left");
-    if (PS4.DownLeft()) Serial.println("Down Left");
-
-    if (PS4.L1()) Serial.println("L1 Button");
-    if (PS4.R1()) Serial.println("R1 Button");
-
-    if (PS4.Share()) Serial.println("Share Button");
-    if (PS4.Options()) Serial.println("Options Button");
-    if (PS4.L3()) Serial.println("L3 Button");
-    if (PS4.R3()) Serial.println("R3 Button");
-
-    if (PS4.PSButton()) Serial.println("PS Button");
-    if (PS4.Touchpad()) Serial.println("Touch Pad Button");
-
-    if (PS4.L2()) {
-      Serial.printf("L2 button at %d\n", PS4.L2Value());
+      Heltec.display ->drawString(14,0,"Connected.");
     }
-    if (PS4.R2()) {
-      Serial.printf("R2 button at %d\n", PS4.R2Value());
+    else{
+      Heltec.display ->drawString(14,0,"Disconnected.");
     }
 
-    if (PS4.LStickX()) {
-      Serial.printf("Left Stick x at %d\n", PS4.LStickX());
-    }
-    if (PS4.LStickY()) {
-      Serial.printf("Left Stick y at %d\n", PS4.LStickY());
-    }
     if (PS4.RStickX()) {
-      Serial.printf("Right Stick x at %d\n", PS4.RStickX());
+      steering = map(PS4.RStickX(),-128,127,50,150);
+      
     }
-    if (PS4.RStickY()) {
-      Serial.printf("Right Stick y at %d\n", PS4.RStickY());
+    else{
+        steering = 90;
     }
-
-    if (PS4.Charging()) Serial.println("The controller is charging");
-    delay(2000);
+    myservo.write(steering);
+  Heltec.display->display();
 }
